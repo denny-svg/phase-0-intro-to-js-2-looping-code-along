7bb84957fcd5024db366a04e02bbd574c959f292
denny_svg <sarah.wanjiku@student.moringaschool.com>
2024-01-04 19:06:18 +0300

    Done assignment

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ function writeCards(names, eventName) {
     const thankYouMessages = [];
     
     for (let i = 0; i < names.length; i++) {
-        const message = (`Thankyou, ${names[i]}, for the wonderful ${eventName} gift!`);
-                thankYouMessages.push(message);
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
     }
     
     return thankYouMessages;
-    }
-    
-    function countDown(startingNumber) {
+  }
+  
+  function countDown(startingNumber) {
     let count = startingNumber;
     
     while (count >= 0) {
-        console.log(count);
-        count--;
+      console.log(count);
+      count--;
     }
-    }
-    
-    const thankYouMessages = (writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
-    console.log(thankYouMessages);
-    
-    countDown(10);
+  }
+  
+  const thankYouMessages = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+  console.log(thankYouMessages);
+  
+  countDown(10);
